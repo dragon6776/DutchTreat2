c641cb7ebd305ce77e0f1f838953c5a157c0c422
DRAGON <truongduchuy182@gmail.com>
2018-01-16 21:52:48 +0700

    add new dutchtreat project with full idnetity & database

---
+++ DutchTreat2/DutchTreat/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Models.ManageViewModels
+{
+    public class GenerateRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
